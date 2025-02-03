d0f3cc0d8c6540a1e5501245eb452d38b848c530
Enzo <enzotresmediano@gmail.com>
2025-02-03 08:45:27 -0300

    Resolving git branches and conflicts

--- src/firebase/fire.py
+++ src/firebase/fire.py
@@ -4,6 +4,7 @@ from firebase_admin import credentials, db
 
 # Get the service account key from the Firebase project settings
 # Gear icon > Project settings > Service accounts > Generate new private key
+# Leave serviceAccountKey.json in the same directory as this file
 
 service_account_path = os.path.join(os.path.dirname(__file__), 'serviceAccountKey.json')
 cred = credentials.Certificate(service_account_path)
