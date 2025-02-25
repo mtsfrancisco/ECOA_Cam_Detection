edf22f8549bac3519c4b84e491940e668fd84707
mtsfrancisco <matheusdabmlj@gmail.com>
2025-02-25 12:49:00 -0300

    update bugged

--- face_recognition_admin/users/urls.py
+++ face_recognition_admin/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_user, success_view, index, list_users
+from .views import add_user, success_view, index, list_users, update_user
 
 urlpatterns = [
     path('', index, name='index'),
     path('add/', add_user, name='add_user'),
     path('success/', success_view, name='success'),
     path('list/', list_users, name='list_users'),
+    path("update_user/<str:user_id>/", update_user, name="update_user"),
 ]
